22747172bb5e305e7b4d41b0ca050bfaa64f7c9b
BF20 <112736411+BF20@users.noreply.github.com>
2022-11-05 19:37:39 -0400

    Update scraper.py
    Added get_metro_data function

--- website/scraper.py
+++ website/scraper.py
@@ -79,3 +79,88 @@ def get_nofrills_data(search: str) -> list[dict]:
                            "units": finalunits[i].strip("[]/")})
 
     return final_list
+
+def get_metro_data(search: str) -> list[dict]:
+    # Given a search, returns a list of dictionaries. The dictionaries are written with three entries:
+    # ["name": name[str], "price": price[float], "unit": unit[str]]
+    # "name" is name of item
+    # If no price can be found, then the price is 0.0.
+    # If no units can be found, then unit is "No Units"
+    driver.get('https://www.metro.ca/en/online-grocery/search?filter=' + search)
+    time.sleep(10)
+    source = driver.page_source
+
+
+    # def get_data_from_search()
+
+    soup = BeautifulSoup(source, 'lxml')
+
+    # We need to get the name, price, and quantity (in grams).
+
+    # something = soup.find_all("a")
+
+    item_selector = soup.find_all('div', class_="default-product-tile tile-product item-addToCart")
+
+    name_classes = []
+    price_classes = []
+    #unit_classes = []
+    for i in item_selector:
+        soup = BeautifulSoup(str(i), 'lxml')
+        name_class = soup.find_all('div', class_='head__title')
+        price_class = soup.find_all('div', class_='pricing__secondary-price')
+        unit_class = soup.find_all('span', class_='price__unit comparison-price-list__item__price__unit')
+        name_classes.append(name_class)
+        price_classes.append(price_class)
+        #unit_classes.append(unit_class)
+    print(price_classes)
+
+    names = []
+    price_per_100grams = []
+    units = []
+    for i in name_classes:
+        soup = BeautifulSoup(str(i), 'lxml')
+        names.append(soup.get_text())
+    for i in price_classes:
+        soup = BeautifulSoup(str(i), 'lxml')
+        price_per_100grams.append(soup.get_text())
+    # for i in unit_classes:
+    #     soup = BeautifulSoup(str(i), 'lxml')
+    #     units.append(soup.get_text())
+
+    # This code is to deal with cases when no price or units can be found.
+
+    # Separating prices and units
+    fixed_price_per100grams = []
+    fixed_units = []
+    for element in price_per_100grams:
+        item = element.replace("\n", "").strip("/").split(" ")
+        fixed_price_per100grams.append(item[0])
+        fixed_units.append(item[1])
+
+
+
+
+
+
+    finalprice_per_100grams = []
+
+    for i in fixed_price_per100grams:
+        if i == '[]':
+            finalprice_per_100grams.append('0')
+        else:
+            finalprice_per_100grams.append(i)
+
+    finalunits = []
+    for i in fixed_units:
+        if i == '[]':
+            finalunits.append('No Units')
+        else:
+            finalunits.append(i)
+
+    final_list = []
+    for i in range(0, len(names)):
+        final_list.append({"name": names[i].strip("[]"), "price": float(finalprice_per_100grams[i].strip("[]$")),
+                           "units": finalunits[i].strip("[]/")})
+
+    return final_list
+
