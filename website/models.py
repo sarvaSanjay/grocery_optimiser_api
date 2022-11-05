7fb9136b7f14d4d8136d535aa62fa8cd3ca77652
Sarva Sanjay <sarva.sanjay@mail.utoronto.ca>
2022-11-05 14:49:48 -0400

    Updated models.py

--- website/models.py
+++ website/models.py
@@ -4,7 +4,5 @@ class Orders(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer)
     item = db.Column(db.String(150))
-    base_quantity = db.Column(db.Integer)
+    base_quantity = db.Column(db.Integer, default=100)
     number = db.Column(db.Integer)
-    is_discrete = db.Column(db.Boolean)
-    user_id = db.Column(db.Integer)
