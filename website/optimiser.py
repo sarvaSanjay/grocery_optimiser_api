b12c79eeb0e6c58ed0700ac480c5a13dfe832605
BF20 <112736411+BF20@users.noreply.github.com>
2022-11-05 13:08:35 -0400

    Update optimiser.py

---
+++ website/optimiser.py
@@ -0,0 +1,16 @@
+def optimize(list_of_store_data: list[list[dict[str:list[list]]]]) -> list:
+    for store in list_of_store_data:
+        for item in store:
+            
+
+
+
+
+
+
+
+
+    list_of_items = []
+
+solution = store, estimated_price, list_of_items
+return solution
