c6de057feb258e446f38dc3e95de0b220f3729a8
vedantswamy <117527665+vedantswamy@users.noreply.github.com>
2022-11-06 00:34:32 -0400

    Update optimiser.py

--- website/optimiser.py
+++ website/optimiser.py
@@ -1,16 +1,21 @@
-def optimize(list_of_store_data: list[list[dict[str:list[list]]]], quanities: dict[str:[str, float]) -> list:
-    for store in list_of_store_data:
+import math
+def optimizer(store: dict, quantities: dict[str, float]) -> list:
+    estimated_price = 0
+     list_of_items = []
         for item in store:
-            
-
-
-
-
-
-
-
-
-    list_of_items = []
-
-solution = store, estimated_price, list_of_items
-return solution
+            value = {}
+            for each_item in store[item]:
+                for t, q in each_item:
+                    if t == "unit" and q == "price":
+                        value[each_item] = each_item[q] * quantities[item] / each_item[t]
+                        break
+            x = [value[each] for each in store[item]]
+            r = min(x)
+            for each in store[item]:
+                if value[each] == r:
+                 for y in each:
+                    if y =="Name":
+                        name = each[y]
+            estimated_price += r
+            list_of_items += [{'name':name, 'price':math.round(r,2)}]
+             return  [estimated_price]+list_of_items
